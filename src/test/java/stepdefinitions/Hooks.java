e6828d972a11bbd947743b07c0f039d68de2df3e
hakantetik44 <fildisili88@gmail.com>
2024-09-29 22:25:05 +0200

    web

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -1,15 +1,10 @@
 package stepdefinitions;
-
-import static utils.Driver.getCurrentDriver;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.en.Given;
-
 import java.net.MalformedURLException;
-
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -75,6 +70,12 @@ public class Hooks {
         getCurrentDriver().quit(); */
         WebDriver driver = Driver.getCurrentDriver();
         if (driver != null) {
+            try {
+                // 5 saniye bekleme ekleniyor
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             if (OS.OS.equals("Android")) {
                 killApplication((AndroidDriver) driver);
             }
--- src/test/java/stepdefinitions/menu/AliExpressStepDefinitions.java
+++ src/test/java/stepdefinitions/menu/AliExpressStepDefinitions.java
@@ -1,5 +1,6 @@
 package stepdefinitions.menu;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
@@ -12,18 +13,19 @@ public class AliExpressStepDefinitions {
 
     private AcceuilPage acceuilPage = new AcceuilPage();
 
-    @Given("I am on the AliExpress platform")
+    /*@Given("I am on the AliExpress platform")
     public void iAmOnTheAliExpressPlatform() {
         if (OS.isWeb()) {
             String actualResult = Driver.getCurrentDriver().getCurrentUrl();
             Assert.assertEquals("https://fr.aliexpress.com/?gatewayAdapt=glo2fra", actualResult);
         } else if (OS.isAndroid()) {
-
         }
     }
-
     @When("Je vérifie que title est {string}")
-    public void jeVerifieQueTitleEst(String title) {
+    public void jeVerifieQueTitleEst(String title) throws InterruptedException {
+        acceuilPage.clickBtnNotification();
+        acceuilPage.clickCategories();
+        Thread.sleep(3000);
         String actualResult = Driver.getCurrentDriver().getTitle();
         Assert.assertEquals(title, actualResult);
     }
@@ -35,20 +37,58 @@ public class AliExpressStepDefinitions {
 
     @When("Je clique sur le bouton pour appeler le service client")
     public void jeCliqueSurLeBoutonPourAppelerLeServiceClient() {
-        // acceuilPage.clickCustomerServiceCallButton();
     }
 
     @Then("Je devrais voir l'écran d'appel téléphonique")
     public void jeDevraisVoirLEcranDAppelTelephonique() {
-
-     /*   if (OS.isWeb()) {
-
+        if (OS.isWeb()) {
             Assert.assertTrue("L'écran de confirmation d'appel n'est pas affiché",
                     Driver.getCurrentDriver().findElement(By.cssSelector(".call-confirmation")).isDisplayed());
         } else if (OS.isAndroid()) {
             Assert.assertTrue("L'écran d'appel n'est pas affiché",
                     Driver.getCurrentDriver().findElement(By.id("com.android.dialer:id/dialpad")).isDisplayed());
         }
-    } */
+    }
+    }*/
+
+
+
+
+
+
+
+
+    @Given("L'utilisateur visualise et vérifie la page daccueil Aliexpress")
+    public void lUtilisateurVisualiseEtVérifieLaPageDaccueilAliexpress() {
+        {
+            if (OS.isWeb()) {
+                String actualResult = Driver.getCurrentDriver().getCurrentUrl();
+                Assert.assertEquals("https://fr.aliexpress.com/?gatewayAdapt=glo2fra", actualResult);
+            } else if (OS.isAndroid()) {
+            }
+        }
+    }
+    @When("Je vérifie que title est {string}")
+    public void jeVerifieQueTitleEst(String title) throws InterruptedException {
+        acceuilPage.clickAccepterLesCookies();
+        acceuilPage.clickBtnNotification();
+        String actualResult = Driver.getCurrentDriver().getTitle();
+        Assert.assertEquals(title, actualResult);
+    }
+
+    @Then("L'utilisateur accède à longlet {string}")
+    public void lUtilisateurAccèdeÀLonglet(String arg0) throws InterruptedException {
+        acceuilPage.clickCategories();
+        Thread.sleep(3000);
+    }
+
+    @And("L'utilisateur clique sur la catégorie {string}")
+    public void lUtilisateurCliqueSurLaCatégorie(String arg0) {
+        acceuilPage.clickChaussures();
+    }
+
+    @And("L'utilisateur sélectionne la sous-catégorie {string}")
+    public void lUtilisateurSélectionneLaSousCatégorie(String arg0) {
+        acceuilPage.clickMocassins();
     }
 }
